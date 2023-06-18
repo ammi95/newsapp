bd6c7eea88fb3688c304dc20d1ea3e654554c2c5
Aman <amanjaiswar54@gmail.com>
2023-06-18 21:20:39 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Newsitem from './Newsitem';
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+                this is news Component
+                <Newsitem/>
+                <Newsitem/>
+                <Newsitem/>
+                <Newsitem/>
+                <Newsitem/>
+            </div>
+            // <div className="card" >
+            //     <img src="..." className="card-img-top" alt="..." />
+            //     <div className="card-body">
+            //         <h5 className="card-title">Card title</h5>
+            //         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            //         <a href="/" className="btn btn-primary">Go somewhere</a>
+            //     </div>
+            // </div>
+        );
+    }
+}
+
+export default News;
---
+++ src/components/Newsitem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Newsitem extends Component {
+  render() {
+    return (
+      <div>
+        this is new item skjfhih sdji
+      </div>
+    );
+  }
+}
+
+export default Newsitem;
